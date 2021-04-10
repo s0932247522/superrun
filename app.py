cbc2e69d20a4ff8598785f796ba6c3c4ce2e96d1
BoLin Chen <s0932247522@gmail.com>
2021-04-10 15:18:26 +0800

    HI

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('4nmaVLCrGOH/jc0STWiQmtM0xSeYAS0jbsR0j4/wv8XjhKnS/gT/iPZL81tYtsZhT8TuCICf7rhl7OPypV2qnCpa6l8PrSUUzoR4O4nSEOVtaPirc6GFPQwVAwVwXfbJZSCPn/pzZyuW2uj9NtSGZgdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('IOmxiatB18j0urE8jbRQGxi4Kbrf7ZjVpfVk2svFCh+JoRF45vRe9/wanzNb4j54T8TuCICf7rhl7OPypV2qnCpa6l8PrSUUzoR4O4nSEOXbdyGIoN+Isn5ezMtcn7GkYUJjNQjqDVv+Fc34fTVUVgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('92f35de5943c5deeb8b57bff35e1b3b0')
 
 
@@ -37,9 +37,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text='HI'))
 
 
 if __name__ == "__main__":
