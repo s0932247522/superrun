d4347ec954dadaaa42a1f3a98b8fca50afd89dc9
BoLin Chen <s0932247522@gmail.com>
2021-04-11 20:01:01 +0800

    update

--- app.py
+++ app.py
@@ -77,11 +77,13 @@ def handle_message(event):
         # 開啟該Google sheets
         sh = gc.open_by_url(gs_url)
         ws = sh.worksheet_by_title(week_grades(msg[-1:]))
-
-        g, n, a, d = personal(msg[3:-1])
+        msg_name = msg[3:-1]
+        g, n, a, d = personal(msg_name)
         rate = round(int(n) / int(g) * 100, 2)
         # print(msg[3:-1], g, n, a, d)
         val = msg[3:-1] + '\n目標步數：' + g + '\n當前步數：' + n + '\n是否達成：' + a + '\n還差幾步：' + d + + '\n達成率為：' + rate 
+
+
     
 
 
