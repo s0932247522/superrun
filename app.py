3246c832d016c3f4f2cf8b0bd3117db5b1494fbf
BoLin Chen <s0932247522@gmail.com>
2021-04-11 19:09:43 +0800

    Google Excel search156

--- app.py
+++ app.py
@@ -40,22 +40,22 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def week_grades(week):
-        if week == '1':
-            week_find = 'week1_find'
-        elif week == '2':
-            week_find = 'week2_find'
-        elif week == '3':
-            week_find = 'week3_find'
-        elif week == '4':
-            week_find = 'week4_find'
-        elif week == '5':
-            week_find = 'week5_find'
-        elif week == '6':
-            week_find = 'week6_find'
-        else:
-            week_find = 'week1_find'
+    if week == '1':
+        week_find = 'week1_find'
+    elif week == '2':
+        week_find = 'week2_find'
+    elif week == '3':
+        week_find = 'week3_find'
+    elif week == '4':
+        week_find = 'week4_find'
+    elif week == '5':
+        week_find = 'week5_find'
+    elif week == '6':
+        week_find = 'week6_find'
+    else:
+        week_find = 'week1_find'
 
-        return week_find
+    return week_find
 
 def personal(name):
     for cell in range(2,56):
@@ -76,25 +76,12 @@ def handle_message(event):
         gs_url = 'https://docs.google.com/spreadsheets/d/1mk9luUpS0h2XHZ1p2gKECADIMc-hdAjXQlxPM-9F40U/edit#gid=0'
         # 開啟該Google sheets
         sh = gc.open_by_url(gs_url)
-        ws = sh.worksheet_by_title('week1_find')
-        val = ws.get_value('B4')
-    
+        ws = sh.worksheet_by_title(week_grades(msg[-1:]))
 
-
-   # gc = pygsheets.authorize(service_account_file='superrun.json')
-
-        # # 輸入要更改的Googles Sheets網址（也可直接用 Google Sheets 的 ID ）
-        # gs_url = 'https://docs.google.com/spreadsheets/d/1mk9luUpS0h2XHZ1p2gKECADIMc-hdAjXQlxPM-9F40U/edit#gid=0'
-
-        # # 開啟該Google sheets
-        # sh = gc.open_by_url(gs_url)
-
-        # ws = sh.worksheet_by_title(week_grades(msg[-1:]))
-
-        # g, n, a, d, = personal(msg[3:-1])
-        # rate = round(int(n) / int(g) * 100, 2)
-        # # print(msg[3:-1], g, n, a, d)
-        # val = msg[3:-1] + '\n目標步數：' + g + '\n當前步數：' + n + '\n是否達成：' + a + '\n還差幾步：' + d + + '\n達成率為：' + rate 
+        g, n, a, d, = personal(msg[3:-1])
+        rate = round(int(n) / int(g) * 100, 2)
+        # print(msg[3:-1], g, n, a, d)
+        val = msg[3:-1] + '\n目標步數：' + g + '\n當前步數：' + n + '\n是否達成：' + a + '\n還差幾步：' + d + + '\n達成率為：' + rate 
 
 
     line_bot_api.reply_message(
