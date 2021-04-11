f1d8e4b6ba864d718e35e6d277fa701fc1d73db2
BoLin Chen <s0932247522@gmail.com>
2021-04-11 17:46:01 +0800

    Google Excel search

--- app.py
+++ app.py
@@ -35,17 +35,55 @@ def callback():
 
     return 'OK'
 
+def week_grades(week):
+            if week == '1':
+                week_find = 'week1_find'
+            elif week == '2':
+                week_find = 'week2_find'
+            elif week == '3':
+                week_find = 'week3_find'
+            elif week == '4':
+                week_find = 'week4_find'
+            elif week == '5':
+                week_find = 'week5_find'
+            elif week == '6':
+                week_find = 'week6_find'
+            else:
+                week_find = 'week1_find'
+
+            return week_find
+
+def personal(name):
+            for cell in range(2,56):
+                cel = 'B' + str(cell)
+                if ws.get_value(cel)[11:] == name:
+                    goal = ws.get_value('C' + str(cell))
+                    now = ws.get_value('D' + str(cell))
+                    achieve = ws.get_value('E' + str(cell))
+                    disparity = ws.get_value('F' + str(cell))
+                    return goal, now, achieve, disparity
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    gc = pygsheets.authorize(service_account_file='superrun.json')
-    # 輸入要更改的Googles Sheets網址（也可直接用 Google Sheets 的 ID ）
-    gs_url = 'https://docs.google.com/spreadsheets/d/1mk9luUpS0h2XHZ1p2gKECADIMc-hdAjXQlxPM-9F40U/edit#gid=0'
-    # 開啟該Google sheets
-    sh = gc.open_by_url(gs_url)
-    ws = sh.worksheet_by_title('week1_goal')
-    val = ws.get_value('A8')
+
+    if '超跑隊' in msg:
+        ws = sh.worksheet_by_title(week_grades(msg[-1:]))
+        g, n, a, d, = personal(msg[3:-1])
+        rate = round(int(n) / int(g) * 100, 2)
+        # print(msg[3:-1], g, n, a, d)
+        val = msg[3:-1] + '\n目標步數：' + g + '\n當前步數：' + n + '\n是否達成：' + a + '\n還差幾步：' + d + + '\n達成率為：' + rate
+    else
+        return
+
+    # gc = pygsheets.authorize(service_account_file='superrun.json')
+    # # 輸入要更改的Googles Sheets網址（也可直接用 Google Sheets 的 ID ）
+    # gs_url = 'https://docs.google.com/spreadsheets/d/1mk9luUpS0h2XHZ1p2gKECADIMc-hdAjXQlxPM-9F40U/edit#gid=0'
+    # # 開啟該Google sheets
+    # sh = gc.open_by_url(gs_url)
+    # ws = sh.worksheet_by_title('week1_goal')
+    # val = ws.get_value('A8')
 
 
 
